1ecbd962dd150c8c58f7f9d0f227c155ccf5045a
Jacob Peddicord <jpeddicord@ubuntu.com>
2009-08-25 22:51:03 -0400

    added debian packaging information

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import distutils.core
 
 setup_info = dict(
     name='Mound',
-    version='0.1',
+    version='0.1.0',
     description='Mound Data Manager',
     author='Jacob Peddicord',
     author_email='jpeddicord@ubuntu.com',
